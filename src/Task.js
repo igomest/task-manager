46a9466f96c656db2b379579ebe4ba5796838062
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-10 11:25:27 -0300

    adding task delete functionality

--- src/Task.js
+++ src/Task.js
@@ -1,33 +1,57 @@
 import './task.css'
-import {useState} from 'react'
+import { useState } from 'react'
 import TaskItem from './TaskItem'
 import EditTask from './EditTask'
 
-function Task({id, title, description, completed}) {
+import { doc, updateDoc, deleteDoc } from "firebase/firestore"
+import { db } from "./firebase"
+
+function Task({ id, title, description, completed }) {
 
   const [checked, setChecked] = useState(completed)
-  const [open, setOpen] = useState({edit:false, view:false})
+  const [open, setOpen] = useState({ edit: false, view: false })
 
   const handleClose = () => {
-    setOpen({edit:false, view:false})
+    setOpen({ edit: false, view: false })
   }
 
-   /* function to update document in firestore */
+  /* function to update document in firestore */
+  const handleCheckedChange = async () => {
+    const taskDocRef = doc(db, 'tasks', id)
 
-   /* function to delete a document from firstore */ 
+    try {
+      await updateDoc(taskDocRef, {
+        completed: checked
+      })
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  /* function to delete a document from firstore */
+  const handleDelete = async () => {
+    const taskDocRef = doc(db, 'tasks', id)
+
+    try {
+      await deleteDoc(taskDocRef)
+    } catch (err) {
+      alert(err)
+    }
+  }
 
   return (
     <div className={`task ${checked && 'task--borderColor'}`}>
       <div>
-        <input 
-          id={`checkbox-${id}`} 
+        <input
+          id={`checkbox-${id}`}
           className='checkbox-custom'
-          name="checkbox" 
-          checked={checked} 
+          name="checkbox"
+          checked={checked}
+          onChange={handleCheckedChange}
           type="checkbox" />
-        <label 
-          htmlFor={`checkbox-${id}`} 
-          className="checkbox-custom-label" 
+        <label
+          htmlFor={`checkbox-${id}`}
+          className="checkbox-custom-label"
           onClick={() => setChecked(!checked)} ></label>
       </div>
       <div className='task__body'>
@@ -35,33 +59,33 @@ function Task({id, title, description, completed}) {
         <p>{description}</p>
         <div className='task__buttons'>
           <div className='task__deleteNedit'>
-            <button 
-              className='task__editButton' 
-              onClick={() => setOpen({...open, edit: true})}>
+            <button
+              className='task__editButton'
+              onClick={() => setOpen({ ...open, edit: true })}>
               Edit
             </button>
-            <button className='task__deleteButton'>Delete</button>
+            <button onClick={handleDelete} className='task__deleteButton'>Delete</button>
           </div>
-          <button 
-            onClick={() => setOpen({...open, view: true})}>
+          <button
+            onClick={() => setOpen({ ...open, view: true })}>
             View
           </button>
         </div>
       </div>
 
       {open.view &&
-        <TaskItem 
-          onClose={handleClose} 
-          title={title} 
-          description={description} 
+        <TaskItem
+          onClose={handleClose}
+          title={title}
+          description={description}
           open={open.view} />
       }
 
       {open.edit &&
-        <EditTask 
-          onClose={handleClose} 
-          toEditTitle={title} 
-          toEditDescription={description} 
+        <EditTask
+          onClose={handleClose}
+          toEditTitle={title}
+          toEditDescription={description}
           open={open.edit}
           id={id} />
       }
